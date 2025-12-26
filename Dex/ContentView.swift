542903ab4fcf61a5e489568060f5cc4c745d7d0a
Gustavo Loaiza Robles <loaizatavo@gmail.com>
2025-12-26 11:18:03 -0600

    All Item code replaced with Pokemon code, app runs with no errors

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -12,59 +12,30 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemnon.name, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
         animation: .default)
-    private var pokemnon: FetchedResults<Pokemnon>
+    private var pokedex: FetchedResults<Pokemon>
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(pokemnon) { pokemon in
+                ForEach(pokedex) { pokemon in
                     NavigationLink {
-                        Text(pokemon.name ?? "Unknown")
+                        Text(pokemon.name ?? "no name")
                     } label: {
-                        Text(pokemon.name ?? "Unknown")
+                        Text(pokemon.name ?? "no name")
                     }
                 }
-                .onDelete(perform: deletePokemnon)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
                 ToolbarItem {
-                    Button(action: addPokemnon) {
-                        Label("Add Pokemon", systemImage: "plus")
+                    Button("Add Item", systemImage: "plus") {
+                        
                     }
                 }
-            }
-            Text("Select a Pokemon")
-        }
-    }
-    
-    private func addPokemnon() {
-        withAnimation {
-            let newPokemon = Pokemnon(context: viewContext)
-            newPokemon.name = "Pokemon #\(Int.random(in: 1...1000))"
-            newPokemon.id = Int16.random(in: 1...1000)
-            // Set other properties as needed
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-    
-    private func deletePokemnon(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { pokemnon[$0] }.forEach(viewContext.delete)
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -15,7 +15,7 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         
-        let newPokemon = Pokemnon(context: viewContext)
+        let newPokemon = Pokemon(context: viewContext)
         newPokemon.id = 1
         newPokemon.name = "bulbasaur"
         newPokemon.types = ["grass", "poison"]
@@ -46,9 +46,11 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                print(error)
             }
         })
+
+        container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
