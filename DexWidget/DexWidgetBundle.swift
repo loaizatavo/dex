191874d3f5b6b8f703331a0891effc52fe69101d
Gustavo Loaiza Robles <loaizatavo@gmail.com>
2026-01-21 19:50:55 -0600

    Add widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Gustavo Loaiza Robles on 21/1/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
