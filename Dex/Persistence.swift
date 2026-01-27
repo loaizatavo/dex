b04e738e907c0da7c881a25547738c178f103848
Gustavo Loaiza Robles <loaizatavo@gmail.com>
2026-01-27 10:38:45 -0600

    Use model container for persistence

--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -5,84 +5,30 @@
 //  Created by Gustavo Loaiza Robles on 6/12/25.
 //
 
-import CoreData
+import SwiftData
+import Foundation
 
+@MainActor
 struct PersistenceController {
-    static let shared = PersistenceController()
     
-    //@MainActor
     static var previewPokemon: Pokemon {
-        let context = PersistenceController.preview.container.viewContext
+        let decoder = JSONDecoder()
         
-        let fetchRequest: NSFetchRequest<Pokemon> = Pokemon.fetchRequest()
-        fetchRequest.fetchLimit = 1
+        decoder .keyDecodingStrategy = .convertFromSnakeCase
         
-        // Try to fetch, but if nothing exists yet, create and return sample data
-        if let results = try? context.fetch(fetchRequest), let first = results.first {
-            return first
-        }
+        let pokemonData = try! Data(contentsOf: Bundle.main.url(forResource: "samplepokemon", withExtension: "json")!)
         
-        // Fallback: create sample Pokemon if fetch fails
-        let newPokemon = Pokemon(context: context)
-        newPokemon.id = 1
-        newPokemon.name = "bulbasaur"
-        newPokemon.types = ["grass", "poison"]
-        newPokemon.hp = 45
-        newPokemon.attack = 49
-        newPokemon.defense = 49
-        newPokemon.specialAttack = 65
-        newPokemon.specialDefense = 65
-        newPokemon.speed = 45
-        newPokemon.spriteURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
-        newPokemon.shinyURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
+        let pokemon = try! decoder.decode(Pokemon.self, from: pokemonData)
         
-        return newPokemon
+        return pokemon
     }
     
-    // The thint that controls our sample preview database
-    //@MainActor
-    static let preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
+    // Our sample preview database
+    static let preview: ModelContainer = {
+        let container = try! ModelContainer(for: Pokemon.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
         
-        let newPokemon = Pokemon(context: viewContext)
-        newPokemon.id = 1
-        newPokemon.name = "bulbasaur"
-        newPokemon.types = ["grass", "poison"]
-        newPokemon.hp = 45
-        newPokemon.attack = 49
-        newPokemon.defense = 49
-        newPokemon.specialAttack = 65
-        newPokemon.specialDefense = 65
-        newPokemon.speed = 45
-        newPokemon.spriteURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
-        newPokemon.shinyURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
+        container.mainContext.insert(previewPokemon)
         
-        
-        do {
-            try viewContext.save()
-        } catch {
-            print("Unresolved error \(error)")
-        }
-        return result
+        return container
     }()
-    
-    let container: NSPersistentContainer
-    
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "Dex")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        } else {
-            container.persistentStoreDescriptions.first!.url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.loaizatavo.DexGroup")!.appending(path: "Dex.sqlite")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                print(error)
-            }
-        })
-        
-        container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
-        container.viewContext.automaticallyMergesChangesFromParent = true
-    }
 }
