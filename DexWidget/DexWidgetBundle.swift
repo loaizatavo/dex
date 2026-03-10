888d8ae10a2d3f50654905f9b977401287f62286
Gustavo Loaiza Robles <loaizatavo@gmail.com>
2026-03-09 19:18:57 -0600

    Update widget to use swift data

--- DexWidget/DexWidgetBundle.swift
+++ DexWidget/DexWidgetBundle.swift
@@ -1,16 +1,16 @@
-////
-////  DexWidgetBundle.swift
-////  DexWidget
-////
-////  Created by Gustavo Loaiza Robles on 21/1/26.
-////
 //
-//import WidgetKit
-//import SwiftUI
+//  DexWidgetBundle.swift
+//  DexWidget
 //
-//@main
-//struct DexWidgetBundle: WidgetBundle {
-//    var body: some Widget {
-//        DexWidget()
-//    }
-//}
+//  Created by Gustavo Loaiza Robles on 21/1/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
